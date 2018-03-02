94378b44f2863fffa57a59e48c392058ca7efcf4
takuya.nakayasu <takuya.nakayasu@accenture.com>
2018-03-02 16:55:18 +0900

    double click counter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,8 +11,12 @@ export class AppComponent implements OnInit {
 
   ngOnInit() {
     Observable.fromEvent(document, 'click')
-    .map(_ => 1)
-    .scan((acc, value) => acc + value)
-    .subscribe(value => this.value = value);
+      .bufferTime(250) // 250ms以内のイベントを配列にまとめる
+      .map(x => x.length) // 配列を配列の長さに変換
+      .filter(length => length >= 2)
+      .do(_ => console.log('double click'))
+      .map(_ => 1)
+      .scan((acc, value) => acc + value)
+      .subscribe(value => this.value = value);
   }
 }
